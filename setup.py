fa68d61ee046388478bb96ab69f556d0f02fdbdd
emergent-reasons <emergentreasons@gmail.com>
2019-01-04 12:30:24 +0900

    version bump to 0.2.0 for changed cli interface

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='pycashaccount',
-    version='0.1.3',
+    version='0.2.0',
     author='emergent-reasons',
     author_email='emergentreasons@gmail.com',
     description='helper for creating cash accounts',
